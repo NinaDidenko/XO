57b3ea935539c90a89e60a18b98485efb10f9949
NinaDidenko <45143024+NinaDidenko@users.noreply.github.com>
2018-11-18 15:35:36 +0300

    Add files via upload

--- xo.js
+++ xo.js
@@ -32,46 +32,51 @@ var counterXO = function(){
 	cellCounterX = [0,0,0];
 	rowCounterO = [0,0,0];
 	cellCounterO = [0,0,0];
-	diagDownCounterX;
-	diagUpCounterX;
-	diagDownCounterO;
-	diagUpCounterO;
+	diagDownCounterX = 0;
+	diagUpCounterX = 0;
+	diagDownCounterO = 0;
+	diagUpCounterO = 0;
 	for (let i = 0; i < 3; i++){
 	for (let j = 0; j < 3; j++){
 		if(tabl[i][j]=="x"){
 		rowCounterX[i]++;
 		cellCounterX[j]++;
-		if(i==j){diagDownCounter++;}else if(j==2-i){diagUpCounterX++);	
+		if(i==j){diagDownCounterX++;}else if(j==(2-i)){diagUpCounterX++};	
 		};
 		if(tabl[i][j]=="0"){
 		rowCounterO[i]++;
-		cellCounterO[j]++;	
+		cellCounterO[j]++;
+		if(i==j){diagDownCounterO++;}else if(j==(2-i)){diagUpCounterO++};		
 		};
 	}
 }
-outXO();
-}
-
-var outXO = function(){
-for (let i = 0; i < 3; i++){
-	if ((rowCounterX[i]==3)||(cellCounterX[i])==3)){
-		win=true;
-	}else if((rowCounterO[i]==3)||(cellCounterO[i])==3)){
-			lose=true;
-				}else if((rowCounterX[i]==3)||(cellCounterX[i])==3)){
-
-					}
-	
-console.log(rowCounterX[i]);
-console.log(rowCounterO[i]);
-console.log(cellCounterX[i]);
-console.log(cellCounterO[i]);	
-}
+answer();
 }
 
 var answer = function(){
+for (let i = 0; i < 3; i++){
+	if ((rowCounterX[i]==3)||(cellCounterX[i]==3)||(diagDownCounterX==3)||(diagUpCounterX==3)){
+		win=true;
+	}else if((rowCounterO[i]==3)||(cellCounterO[i]==3)||(diagDownCounterO==3)||(diagUpCounterO==3)){
+			lose=true;
+				} /*else if((rowCounterX[i]==3)||(cellCounterX[i])==3)){
 
+					}*/
+	
+/*console.log(rowCounterX[i]);
+console.log(rowCounterO[i]);
+console.log(cellCounterX[i]);
+console.log(cellCounterO[i]);*/
+console.log(diagDownCounterX);
 }
+console.log(diagDownCounterX);
+console.log(win);
+console.log	(lose);
+}
+
+/*var answer = function(){
+
+}*/
 
 
 
